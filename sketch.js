2632fcf04113813958741201cca5d47e3daa6b4d
aayushmanranjan <78651719+aayushmanranjan@users.noreply.github.com>
2021-04-16 20:54:15 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,51 +3,53 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+var dustbin1, paperObject,groundObject;
+var world;
 
-var engine,world;
+function preload()
+{
+	
+}
 
 function setup() {
-	createCanvas(800, 700);
-    rectMode(CENTER);
+	createCanvas(1250, 650);
+	rectMode(CENTER);
+
 
 	engine = Engine.create();
 	world = engine.world;
-   
 	
-	
-	paper = new Paper(100,600,10);
+	paperObject=new Paper(200,450,70);
+	groundObject=new Ground(width/2,620,width,20);
+	dustbin1=new Dustbin(1200,510,20,200);
+	dustbin2=new Dustbin(1000,510,20,200);
+	dustbin3=new Dustbin(1100,600,200,20);
+	//Create a Ground
 
-	ground = new Ground(400,680,800,20),
-	
-	leftSide = new Dustbin(550,620,20,100);
-	bottom = new Dustbin(610,660,100,20);
-	rightSide = new Dustbin(670,620,20,100);
-	
+
 	Engine.run(engine);
-	
+  
 }
 
 
 function draw() {
   rectMode(CENTER);
-  background(0);
+  background(230);
  
-  Engine.update(engine);
   
- ground.display();
- paper.display();
- leftSide.display();
- bottom.display();
- rightSide.display();
+  paperObject.display();
+  groundObject.display();
+  dustbin1.display();
+  dustbin2.display();
+  dustbin3.display();
+}
 
- drawSprites();
- 
-} 
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
 
-function KeyPressed(){
-	if (KeyCode === UP_ARROW){
-		Matter.Body.applyForce(paper.body,paper.body.position,{x:15,y:-15});
-		
+	  Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:130,y:-145});
+
+  
 	}
 }
 
